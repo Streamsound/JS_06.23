6e61cc0e3294971a32c68198e25840ce731ed2fc
Stream <job.gerasimov@gmail.com>
2023-05-31 00:16:38 +0200

    filter fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function getDistance (x1, y1, x2, y2) {
     for (let i = 0; i < 4; i++) {
-        if (typeof arguments[i] === undefined || isNaN (arguments[i])) {
+        if (typeof arguments[i] !== "number" || isNaN (arguments[i]) || typeof arguments[i] === undefined) {
             throw new Error("Not a number")
         } else if (arguments[i] === Infinity || arguments[i] === -Infinity){
             throw new Error("Not correct number.")
@@ -8,6 +8,7 @@ function getDistance (x1, y1, x2, y2) {
             throw new Error ("Not in the frames")
         }
     }
+
 }
 
-getDistance(1,2,3)
+getDistance(1,2,4, -500)
