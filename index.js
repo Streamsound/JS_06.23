555cb66183332a3433b0027647dd760d3b2f75aa
Stream <job.gerasimov@gmail.com>
2023-05-31 00:09:54 +0200

    arguments check

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function getDistance (x1, y12, x2, y2) {
-    
+function getDistance (x1, y1, x2, y2) {
+    for (let i = 0; i < 4; i++) {
+        if (typeof arguments[i] === undefined || isNaN (arguments[i])) {
+            throw new Error("Not a number")
+        } else if (arguments[i] === Infinity || arguments[i] === -Infinity){
+            throw new Error("Not correct number.")
+        } else if (arguments[i] > 1000 || arguments[i] < -1000){
+            throw new Error ("Not in the frames")
+        }
+    }
 }
+
+getDistance(1,2,3)
