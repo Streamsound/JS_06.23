8f68c48472e7eefc941099a9573465f457ee0ffe
Stream <job.gerasimov@gmail.com>
2023-05-31 15:15:05 +0200

    2 task done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function getDistance (x1, y1, x2, y2) {
+/*function getDistance (x1, y1, x2, y2) {
     for (let i = 0; i < 4; i++) {
         if (typeof arguments[i] !== "number" || isNaN (arguments[i]) || typeof arguments[i] === undefined) {
             throw new Error("Not a number")
@@ -13,5 +13,29 @@ function getDistance (x1, y1, x2, y2) {
         return distance
     } else {
         return distance.toFixed(2)
+    }
+}
+*/
+
+function switchPlaces(arr) {
+    if(Array.isArray(arr)) {
+        if (arr.length === 0) {
+            return []
+        } else {
+            let result = []
+            let middle = arr[Math.round((arr.length - 1) / 2)]
+            if (arr.length % 2 === 0) {
+                let first = arr.slice(0, arr.indexOf(middle))
+                let second = arr.slice(arr.indexOf(middle))
+                result = second.concat(first)
+                return result
+            } else {
+                let first = arr.slice(0, arr.indexOf(middle))
+                let second = arr.slice(arr.indexOf(middle) + 1)
+                result = second.concat(middle,first)}
+                return result
+        }
+    } else {
+        throw new Error("Not array")
     }
 }
